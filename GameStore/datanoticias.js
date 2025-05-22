8fbb74bf5a92b2356da45b2df6b19e6ee4513ba8
Diego Rodriguez <20204879@aloe.ulima.edu.pe>
2025-05-22 15:17:42 -0500

    Add noticias js

---
+++ GameStore/datanoticias.js
@@ -0,0 +1,19 @@
+// newsData.js
+
+const defaultNews = [
+  {
+    title: "GTA VI será lanzado en 2025",
+    content: "Rockstar Games confirmó oficialmente el lanzamiento de Grand Theft Auto VI.",
+    image: "images/gta6.jpg"
+  },
+  {
+    title: "Spider-Man 2 rompe récords",
+    content: "Más de 5 millones de copias en la primera semana.",
+    image: "images/spidy.jpg"
+  },
+  {
+    title: "Nuevo avance de God of War",
+    content: "Sony muestra nuevo contenido de God of War en el State of Play.",
+    image: "images/godofwar.png"
+  }
+];
